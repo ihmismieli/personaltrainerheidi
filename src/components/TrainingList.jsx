eba842feb308e786e3668f38542d8b822d98a2e0
Heidi Ahlgren <heidiahlgren@hotmail.com>
2024-12-02 20:37:34 +0200

    Statics removed from traininglist.jsx

--- src/components/TrainingList.jsx
+++ src/components/TrainingList.jsx
@@ -7,7 +7,6 @@ import "ag-grid-community/styles/ag-grid.css";
 import "ag-grid-community/styles/ag-theme-material.css";
 import { Button, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle, Snackbar, Typography } from "@mui/material";
 import AddTraining from "./AddTraining";
-import Statics from "./Statics";
 
 
 export default function TrainingList() {
@@ -81,7 +80,6 @@ export default function TrainingList() {
     return (
         <>
             <AddTraining handleFetch={handleFetch} />
-            <Statics trainings={trainings} handleFetch={handleFetch}/>
             <Typography variant="h5" style={{ margin: 10 }}>Trainings</Typography>
             <div className="ag-theme-material" style={{ height: 500, width: '100%' }}>
                 <AgGridReact
